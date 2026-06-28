cc3b96e968cd6bd81b75b7b6b6477a55da0b392f
Samir P Nimgade <samirnimgade76@gmail.com>
2026-06-28 23:49:33 +0530

    Update Skill-Scikit5.py

--- Skill-Scikit5.py
+++ Skill-Scikit5.py
@@ -1,4 +1,4 @@
-#Lower error using XGBoost
+#Lower error using LightGBM
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
